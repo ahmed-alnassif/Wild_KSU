778cf66c69abb6b7722f1964e45a59df15605a0c
pershoot <190600+pershoot@users.noreply.github.com>
2026-01-17 20:17:36 -0500

    kernel: Prune legacy (4.x) support (https://github.com/KernelSU-Next/KernelSU-Next/pull/1072)

--- kernel/arch.h
+++ kernel/arch.h
@@ -18,13 +18,17 @@
 #define __PT_SP_REG sp
 #define __PT_IP_REG pc
 
+<<<<<<< HEAD
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 16, 0)
+=======
+>>>>>>> 56547eab (kernel: Prune legacy (4.x) support)
 #define REBOOT_SYMBOL "__arm64_sys_reboot"
 #define SYS_READ_SYMBOL "__arm64_sys_read"
 #define SYS_EXECVE_SYMBOL "__arm64_sys_execve"
 // https://cs.android.com/android/kernel/superproject/+/common-android-mainline:common/scripts/syscalltbl.sh;l=57;drc=9142be9e6443fd641ca37f820efe00d9cd890eb1
 // https://cs.android.com/android/kernel/superproject/+/common-android-mainline:common/scripts/syscall.tbl;l=104;drc=b36d4b6aa88ef039647228b98c59a875e92f8c8e
 #define SYS_FSTAT_SYMBOL "__arm64_sys_newfstat"
+<<<<<<< HEAD
 #else
 #define PRCTL_SYMBOL "sys_prctl"
 #define SYS_READ_SYMBOL "sys_read"
@@ -32,6 +36,8 @@
 #define SYS_FACCESSAT_SYMBOL "sys_faccessat"
 #define SYS_EXECVE_SYMBOL "sys_execve"
 #endif
+=======
+>>>>>>> 56547eab (kernel: Prune legacy (4.x) support)
 
 #elif defined(__x86_64__)
 
